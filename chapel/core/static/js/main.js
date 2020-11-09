03d653c922f2556ebb3cdea7095ffa0adb610c12
Vinicius Boscoa <virb30@gmail.com>
2020-11-09 18:00:14 -0300

    add vuejs

---
+++ chapel/core/static/js/main.js
@@ -0,0 +1,39 @@
+/*
+1 - após envio do form:
+    - esconde form
+    - mostra vela
+    - mostra sucesso
+    - mostra botão acende vela
+
+2 - após acende vela
+    - mostra imagem fogo
+    - mostra mensagem vela 7 dias
+
+3 - Clique faça sua prece (recarrega página)
+    - esconde mensagem de sucesso
+    - esconde vela
+    - escode imagem fogo
+    - esconde mensagem vela 7 dias
+    - mostra form
+*/
+
+const virtualCandle = new Vue({
+    el: '#virtual-candle',
+    data: {
+        litCandle: false,
+        success: false,
+    },
+    methods: {
+        hello() {
+            location.reload();
+        },
+        showCandle(event) {
+            event.preventDefault();
+            this.success = true;
+            this.litCandle = false;
+        },
+        lightCandle() {
+            this.litCandle = true;
+        },
+    }
+});
